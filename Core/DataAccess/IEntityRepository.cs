3c26fb274cb2fe6f12e6169122ede7824ad53563
Elif PİRGAİP <elifpirgaip27@gmail.com>
2021-03-09 18:29:43 +0300

    09.03.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,17 +1,17 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // Generic constraint : generic kısıtlama.
     // class : referans tip olabilir anlamına gelir, aşağıdaki "T:class" ifadesi.
     // T:class,IEntity : IEntity ya da IEntity implemente eden bir nesne olabilir anlamına gelir.
     // T:class, IEntity, new() : newlenebilir olmalı anlamına geldiğinden ve interface'ler new'lenemediğinden, IEntity'i kullandığımızda hata verir.
     // Böylelikle sistemimiz yalnızca veritabanı nesneleri ile çalışan bir repository oldu. 
-    public interface IEntitiyRepository<T> where T:class,IEntity,new()
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null); // filtremeler yapabilmek için linq.expression kütüphanesini eklemeliyiz.
         T Get(Expression<Func<T, bool>> filter);
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,8 +8,11 @@ using System.Text;
 namespace DataAccess.Abstract
 {
     // Not: Interface'lerin "operasyonları" Public'tir.
-    public interface IProductDal:IEntitiyRepository<Product>
+    public interface IProductDal:IEntityRepository<Product>
     {
-        
+        List<ProductDetailDto> GetProductDetails();
     }
 }
+
+
+// Code Refactoring : Kod iyileştirme.
